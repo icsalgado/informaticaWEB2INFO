bb6812fbacdd3fdd9c212077a735b01e5b406db3
icsalgado <icsalgado@icloud.com>
2021-11-04 09:50:15 -0300

    Completando o código

--- ProfSandra_PHP/aula4/template/recebe.php
+++ ProfSandra_PHP/aula4/template/recebe.php
@@ -1,9 +1,11 @@
 <?php
 
 $name = filter_input(INPUT_GET, 'nome');//filter_input é uma função do php que captura o campo do formulário e verifica se ele está preenchido
+$age = filter_input(INPUT_GET, 'idade');//input_post quando o metodo de envio for POST
 
-if ($name){
+if ($name && $age){
     echo "O nome foi ".$name;
+    echo "<br>A idade foi ".$age;
 }else{
-    echo "nome não informado";
+    echo "um dos campos ou ambos não foram informados";
 }
